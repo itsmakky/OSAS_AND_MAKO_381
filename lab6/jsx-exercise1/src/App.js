a0c73e224fe05e3a228a363d2af9ab09e345ea75
uosasekhator <osadebamwen.ekhator@ucalgary.ca>
2025-03-07 12:21:42 -0700

    Add files via upload

---
+++ lab6/jsx-exercise1/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  let isLoggedIn = true;
+  return (
+    <div className="App">
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>{currentYear}</p>
+      <p>{isLoggedIn ? "Welcome back!":"Please log in."}</p>
+    </div>
+  ); 
+}
+
+export default App;
